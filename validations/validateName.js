ada9d02d01d649c97cda8b1f4b386fba05136db2
Liubov <liubovusikova1@gmail.com>
2023-11-03 10:03:26 +0200

    fix

--- validations/validateName.js
+++ validations/validateName.js
@@ -5,14 +5,14 @@ function validateName(name) {
 }
 
 const validateNameMiddleware = async (req, res, next) => {
-  const { userName } = req.body;
+  const { name } = req.body;
 
-  if (!userName || !validateName(userName)) {
+  if (!name || !validateName(name)) {
     return res.status(400).json({ message: "Invalid name format" });
   }
 
   const existingNameQuery = "SELECT name FROM gamer WHERE name = $1";
-  const result = await pool.query(existingNameQuery, [userName]);
+  const result = await pool.query(existingNameQuery, [name]);
 
   if (result.rows.length > 0) {
     return res.status(400).json({ message: "Name already in use" });
